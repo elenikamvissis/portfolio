3a0de4111850bf9f98192c031d558175424399df
Eleni <eleni.kamvissis@gmail.com>
2023-09-18 10:37:23 +0200

    Loading indicator

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -12,7 +12,10 @@ const Contact: React.FC<{}> = () => {
     const [error, setError] = useState<Array<string>>([]);
     const [success, setSucess] = useState<boolean>(false);
 
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
     const handleSubmit = async (event: React.SyntheticEvent) => {
+        setIsSubmitting(true);
         event.preventDefault();
 
         const res = await fetch('api/contact', {
@@ -24,6 +27,7 @@ const Contact: React.FC<{}> = () => {
         const { msg, success } = await res.json();
         setError(msg);
         setSucess(success);
+        setIsSubmitting(false);
 
         if (success) {
             setName('');
@@ -51,8 +55,22 @@ const Contact: React.FC<{}> = () => {
                             <textarea rows={4} id="message" required value={message} onChange={(e) => setMessage(e.target.value)} />
                         </div>
                         <div>
-                            <button type="submit" className="px-4 py-2 w-48 bg-zinc-600 text-white font-medium rounded-md">
-                                Send message
+                            <button type="submit" className="px-4 py-2 md:w-80 bg-pink-800 text-white font-medium rounded-md" disabled={isSubmitting}>
+                                {isSubmitting ? (
+                                    <div className="flex flex-row items-center justify-center">
+                                        <svg className="animate-spin h-4 w-4 mr-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                                            <path
+                                                className="opacity-75"
+                                                fill="currentColor"
+                                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                            />
+                                        </svg>
+                                        Sending message . . .
+                                    </div>
+                                ) : (
+                                    <div>Send message</div>
+                                )}
                             </button>
                         </div>
                     </form>
