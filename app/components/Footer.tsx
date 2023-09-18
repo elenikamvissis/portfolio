4425b780cc4395b3fefd944166741a95fea6f2be
Eleni <eleni.kamvissis@gmail.com>
2023-09-18 08:47:43 +0200

    Various footer css fixes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,9 +6,7 @@ const Footer: React.FC<{}> = () => {
         <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
             <hr className="w-full h-0.5 mx-auto mt-8 bg-pink-500 border-0"></hr>
             <div className="mx-auto  p-4 flex flex-col text-center md:flex-row md:justify-between">
-                <div className="flex flex-row items-center justify-center space-x-1 text-neutral-700">
-                    © 2023 Eleni Kamvissis<a href="/" className="hover:underline"></a>
-                </div>
+                <div className="flex flex-row items-center justify-center space-x-1 text-neutral-700">© 2023 Eleni Kamvissis</div>
                 <div className="flex flex-row items-center justify-center space-x-2 mb-1">
                     <a href="https://github.com/elenikamvissis" rel="noreferrer" target="_blank">
                         <AiOutlineGithub className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-700" size={30} />
