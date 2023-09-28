0e11f45ea92017fab27c4424238f0f8916d30813
Eleni <eleni.kamvissis@gmail.com>
2023-09-28 09:26:09 +0200

    Home on logo click

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -24,12 +24,14 @@ const NavBar: React.FC = () => {
         <header className="w-full md:px-12 mx-auto px-4 bg-pink-50 shadow md:shadow-none md:bg-gradient-to-b md:from-pink-100 md:to-white fixed top-0 z-50 xs:px-20">
             <div className="md:flex md:items-center justify-between">
                 <div className="flex items-center justify-between py-3 ">
-                    <div className="flex flex-row items-center">
-                        <Image src="/logo.jpeg" alt="Picture of me" width={50} height={50} className="rounded-full shadow-sm shadow-slate-500/50 mr-2" />
-                        <div className="md:py-5 flex flex-row align-middle">
-                            <h1 className="text-xl font-bold text-gray-900">Eleni Kamvissis</h1>
+                    <Link href="/">
+                        <div className="flex flex-row items-center">
+                            <Image src="/logo.jpeg" alt="Logo" width={50} height={50} className="rounded-full shadow-sm shadow-slate-500 mr-2" />
+                            <div className="md:py-5 flex flex-row align-middle">
+                                <h1 className="text-xl font-medium text-gray-900">Eleni Kamvissis</h1>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                     <div className="md:hidden">
                         <button onClick={() => setNavBar(!navBar)}>{navBar ? <AiOutlineClose size={28} /> : <RxHamburgerMenu size={28} />} </button>
                     </div>
@@ -39,7 +41,7 @@ const NavBar: React.FC = () => {
                         <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 text-gray-900">
                             {NAV_ITEMS.map((item, index) => {
                                 return (
-                                    <Link key={index} href={item.page} className={'block lg:inline-block text-gray-900  hover:text-pink-600'} onClick={() => setNavBar(!navBar)}>
+                                    <Link key={index} href={item.page} className={'block lg:inline-block text-gray-900 hover:text-pink-600'} onClick={() => setNavBar(!navBar)}>
                                         {item.label}
                                     </Link>
                                 );
